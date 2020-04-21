6f1461d5e7854d2be74560fc886cd9067232cd4a
Einlanzerous <einlanzerous@live.com>
2020-04-20 21:20:18 -0400

    Fireship and arjunyel project
    Neat little code example, playing around with GraphQL. Utilizing SpceX api.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LaunchDetailsComponent } from './launch-details/launch-details.component';
+import { LaunchListComponent } from './launch-list/launch-list.component';
+import { GraphQLModule } from './graphql.module';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule } from '@angular/material/card';
+import { RelativeTimePipe } from './relative-time/relative-time.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LaunchDetailsComponent,
+    LaunchListComponent,
+    RelativeTimePipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    GraphQLModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
